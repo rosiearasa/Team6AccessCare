131d773d21163a5eca032e66f84e2974ae5caa14
rosiearasa <rosearasa7522@gmail.com>
2020-07-23 08:54:19 -0400

    data added and some search function

---
+++ datak.js
@@ -0,0 +1,63 @@
+let datak =[
+{
+country:"Kenya",
+name:"Safari Doctors",
+region:"Lamu",
+organisation:"Do Right",
+//contactInformation:"",
+
+
+},
+{
+  country:"Kenya",
+  name:"Lewa Conservation Clinic",
+region:"Isiolo",
+organisation:"Do Right",
+//contactInformation:"",
+
+
+},
+{
+  country:"Kenya",
+  name:"Leparau Healthcare Clinic",
+  region:"Isiolo",
+  organisation:"Do Right",
+//  contactInformation:"",
+
+},
+{
+  country:"Kenya",
+  name:"Kakuma Mission Hospital",
+  region:"Turkana",
+  organisation:"Do Right",
+ // contactInformation:"",
+
+},
+{
+  country:"Kenya",
+  name:"Kenya Fluorspar Medical Centre",
+  region:"Elgeyo-Marakwet",
+  organisation:"Do Right",
+  //contactInformation:"",
+
+},
+
+
+{
+  country:"Kenya",
+  name:"Gundua Health Center",
+  region:"Turkana",
+  organisation:"Do Right",
+  //contactInformation:"",
+
+},
+{
+  country:"Kenya",
+  name:"Shella Health Clinic",
+  region:"Lamu",
+  organisation:"Do Right",
+  //contactInformation:"",
+
+},
+
+];
